8cef7fc8116fced1551e1229545d701c0685a2b2
Rene Ramirez <rene.ramirez@4pixels.co>
2016-09-13 17:27:30 -0400

    XML and Json Week 1 and Weather API week 2

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by asus on 06/09/16.
+ */
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    }
+  });
+
+  gulp.watch("app/**/*.html").on('change', browserSync.reload);
+
+
+});
+
+gulp.task('default',['browser-sync']);
